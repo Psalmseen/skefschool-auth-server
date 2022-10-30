953ca40c5fe1139ee0cb1e0d7bc99835b6fddbd0
psalmseen <samsonoyebamiji02@gmail.com>
2022-10-30 20:17:19 +0100

    made minor fixes

--- functions/OAuthServer.ts
+++ functions/OAuthServer.ts
@@ -14,7 +14,12 @@ env.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173', 'https://skefschool.netlify.app'],
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Welcome to SKEF school OAuth server');
